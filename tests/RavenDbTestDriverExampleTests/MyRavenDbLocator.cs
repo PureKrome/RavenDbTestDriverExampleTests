1a3f63819170f62ed65439fd1c95caffb3ad06a4
Pure Krome <github@world-domination.com.au>
2018-01-20 15:06:22 +1000

    sdfdsf

--- tests/RavenDbTestDriverExampleTests/MyRavenDbLocator.cs
+++ tests/RavenDbTestDriverExampleTests/MyRavenDbLocator.cs
@@ -4,15 +4,42 @@ using Raven.TestDriver;
 
 namespace RavenDbTestDriverExampleTests
 {
-    public class MyRavenDbLocator : RavenServerLocator
+    public class CustomRavenServerLocator : RavenServerLocator
     {
         private string _serverPath;
-        private string _command = "dotnet";
-        private string _arguments;
 
-        public override string ServerPath => "C:\\RavenDb\\RavenDB-4.0.0-rc-40025-windows-x64\\Server\\Raven.Server.dll";
+        public override string ServerPath
+        {
+            get
+            {
+                if (!string.IsNullOrWhiteSpace(_serverPath))
+                {
+                    return _serverPath;
+                }
 
-        public override string Command => _command;
+                // Lets look for the exe or dll via (in order):
+                // 1 - environmental variable.
+                // 2 - hardcoded location.
+
+
+                var environmentalVariable = Environment.GetEnvironmentVariable("RavenServerTestPath");
+                var serverPath = !string.IsNullOrWhiteSpace(environmentalVariable)
+                                     ? environmentalVariable
+                                     : "C:\\RavenDb\\RavenDB-4.0.0-rc-40025-windows-x64\\Server\\Raven.Server.dll";
+
+                // Given a serverPath - does something actually exist?
+                if (File.Exists(serverPath))
+                {
+                    _serverPath = serverPath;
+                    return _serverPath;
+                }
+
+                throw new Exception($"Failed to find the location of a raven.server.exe/dll. Tried checking for: {serverPath}. Unable to run any tests :/");
+            }
+        }
+
+        public override string Command => "dotnet";
+        //public override string CommandArguments => ServerPath + " --ServerUrl=http://127.0.0.1:0 --RunInMemory=true";
         public override string CommandArguments => ServerPath;
     }
 }
